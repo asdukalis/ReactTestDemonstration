887975eaa95ff329cb4689d3a6880bd2dbc0d278
asdukalis@gmail.com <asdukalis@gmail.com>
2018-12-03 21:39:48 +0200

    first commit

---
+++ precache-manifest.c19eb41364b7efae8b601e50ba2e226b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5bc6537ad4b59b153ddd",
+    "url": "/static/js/main.5bc6537a.chunk.js"
+  },
+  {
+    "revision": "759e021a08929683516c",
+    "url": "/static/js/1.759e021a.chunk.js"
+  },
+  {
+    "revision": "5bc6537ad4b59b153ddd",
+    "url": "/static/css/main.92b8f702.chunk.css"
+  },
+  {
+    "revision": "c4e03c15713684b2fe683053163359e3",
+    "url": "/index.html"
+  }
+];
